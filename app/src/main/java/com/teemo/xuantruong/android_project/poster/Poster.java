789c57c475ce4b8da8ace7bf640920d65f8cd024
long <duclongph1997@gmail.com>
2018-10-18 08:28:53 +0700

    [project] longndse04571

--- app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
+++ app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
@@ -3,14 +3,60 @@ package com.teemo.xuantruong.android_project.poster;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.teemo.xuantruong.android_project.R;
 
-public class Poster extends AppCompatActivity {
+import java.util.Locale;
+
+public class Poster extends AppCompatActivity implements View.OnClickListener{
     TextToSpeech speak;
+    int  result;
+    EditText edit;
+    String text;
+    ImageButton imageBut ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poster);
+        imageBut= (ImageButton) findViewById(R.id.imageBut);
+        edit= (EditText) findViewById(R.id.edit);
+
+        speak= new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i == TextToSpeech.SUCCESS)
+                {
+                    result= speak.setLanguage(Locale.UK);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"feature not support  in your device",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        imageBut.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()== R.id.imageBut)
+        {
+            text= edit.getText().toString().trim();
+            speak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (speak !=null)
+        {
+            speak.stop();
+            speak.shutdown();
+        }
+        super.onDestroy();
     }
 }
