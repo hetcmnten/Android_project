9552d737d52c32a176a7835b0bde2009b4c87b00
long <duclongph1997@gmail.com>
2018-10-18 15:16:46 +0700

    [project] longndse04571

---
+++ app/src/main/java/com/teemo/xuantruong/android_project/entity/Poster_entity.java
@@ -0,0 +1,53 @@
+package com.teemo.xuantruong.android_project.entity;
+
+public class Poster_entity {
+
+    private  String id_poster;
+    private  String name_poster;
+    private  String header_text;
+    private  String image_Poster;
+    private  String content_poster;
+
+    public Poster_entity(){
+    }
+
+    public String getId_poster() {
+        return id_poster;
+    }
+
+    public void setId_poster(String id_poster) {
+        this.id_poster = id_poster;
+    }
+
+    public String getName_poster() {
+        return name_poster;
+    }
+
+    public void setName_poster(String name_poster) {
+        this.name_poster = name_poster;
+    }
+
+    public String getHeader_text() {
+        return header_text;
+    }
+
+    public void setHeader_text(String header_text) {
+        this.header_text = header_text;
+    }
+
+    public String getImage_Poster() {
+        return image_Poster;
+    }
+
+    public void setImage_Poster(String image_Poster) {
+        this.image_Poster = image_Poster;
+    }
+
+    public String getContent_poster() {
+        return content_poster;
+    }
+
+    public void setContent_poster(String content_poster) {
+        this.content_poster = content_poster;
+    }
+}
--- app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
+++ app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
@@ -3,33 +3,47 @@ package com.teemo.xuantruong.android_project.poster;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.teemo.xuantruong.android_project.R;
+import com.teemo.xuantruong.android_project.connectJson.MyHttpHandler;
+import com.teemo.xuantruong.android_project.entity.Poster_entity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Locale;
 
 public class Poster extends AppCompatActivity implements View.OnClickListener{
-    TextToSpeech speak;
-    int  result;
-    EditText edit;
-    String text;
-    ImageButton imageBut ;
-    String url="";
+    private String TAG = Poster.class.getSimpleName();
+    private TextToSpeech speak;
+    private int  result;
+    private EditText edit;
+    private String text;
+    private ImageButton imageBut ;
+    private String url="";
+    private MyHttpHandler myhttp = new MyHttpHandler();
+    private TextView name, content;
+    private Poster_entity poster_entity =  new Poster_entity();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poster);
         imageBut= (ImageButton) findViewById(R.id.imageBut);
         edit= (EditText) findViewById(R.id.edit);
-
+        name= (TextView) findViewById(R.id.name);
+        content= (TextView) findViewById(R.id.content);
         MediaPlayer mediaPlayer= new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
@@ -54,7 +68,11 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
             }
         });
         imageBut.setOnClickListener(this);
+        MyAsyncTask myAsyncTask = new  MyAsyncTask();
+        myAsyncTask.execute();
+
     }
+
 
     @Override
     public void onClick(View view) {
@@ -62,7 +80,8 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
         {
             text= edit.getText().toString().trim();
             speak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
-
+            name.setText(poster_entity.getName_poster());
+            content.setText(poster_entity.getContent_poster());
         }
     }
 
@@ -75,9 +94,48 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
         }
         super.onDestroy();
     }
-    private  void startTexttoSpeech()
-    {
 
+    // json ->  object
+    class  MyAsyncTask extends AsyncTask<Void,Void,Void>{
+
+
+            @Override
+        protected Void doInBackground(Void... voids) {
+
+                String url1 = "http://172.25.11.198:8082/select_element.php?limit=10002";
+                String jsonStr= myhttp.makeServiceCall(url1);
+
+                if (jsonStr != null) {
+                    try {
+                        JSONArray aray = new JSONArray(jsonStr);
+                        for (int i = 0; i < aray.length(); i++) {
+                            JSONObject object = aray.getJSONObject(i);
+                            String id = object.getString("phone");
+                            String name= object.getString("name");
+                            poster_entity.setName_poster(id);
+                            poster_entity.setContent_poster(name);
+                        }
+                } catch ( final JSONException e) {
+                        Log.e(TAG, "Json parsing error: " + e.getMessage());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {Toast.makeText(getApplicationContext(), "Json parsing error: " + e.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                } else {
+                    Log.e(TAG, "Couldn't get json from server.");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Couldn't get json from server. Check LogCat for possible errors!",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                return null;
+        }
     }
 
 }
