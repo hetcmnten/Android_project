e445de0035f116fdc371f229f7db477021b0aa53
long <duclongph1997@gmail.com>
2018-10-22 20:40:32 +0700

    long

--- app/src/main/java/com/teemo/xuantruong/android_project/connectJson/Get_apiText_to_speech.java
+++ app/src/main/java/com/teemo/xuantruong/android_project/connectJson/Get_apiText_to_speech.java
@@ -21,10 +21,10 @@ public class Get_apiText_to_speech {
     }
 
     // use api  -> read json ( mp3)
-    public  String apiChangetexttomp3() throws  IOException{
+    public  String apiChangetexttomp3(String text) throws  IOException{
         OkHttpClient client = new OkHttpClient();
         MediaType mediaType = MediaType.parse("application/octet-stream");
-        RequestBody body = RequestBody.create(mediaType, "Đúng một tháng trôi qua, người dân Hà Nội và đặc biệt là những cư dân sống ở khu vực Đê La Thành vẫn chưa hết kinh hoàng khi nói về vụ cháy dữ dội xảy ra gần Bệnh viện Nhi Trung ương.\r\n\r\nNgọn lửa dữ dội đã thiêu sạch toàn bộ 19 ngôi nhà gần đó cùng rất nhiều tài sản có giá trị của các hộ kinh doanh trên địa bàn, bao gồm cả những dãy nhà cho bệnh nhân ngoại trú, điều trị dài ngày thuê gần Bệnh viện Nhi.Đúng một tháng trôi qua, người dân Hà Nội và đặc biệt là những cư dân sống ở khu vực Đê La Thành vẫn chưa hết kinh hoàng khi nói về vụ cháy dữ dội xảy ra gần Bệnh viện Nhi Trung ương.\r\n\r\nNgọn lửa dữ dội đã thiêu sạch toàn bộ 19 ngôi nhà gần đó cùng rất nhiều tài sản có giá trị của các hộ kinh doanh trên địa bàn, bao gồm cả những dãy nhà cho bệnh nhân ngoại trú, điều trị dài ngày thuê gần Bệnh viện Nhi.");
+        RequestBody body = RequestBody.create(mediaType, text);
         Request request = new Request.Builder()
                 .url("http://api.openfpt.vn/text2speech/v4?aip_key=e8b586d28b904fd290f813ef6ee05764&voice=female")
                 .post(body)
--- app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
+++ app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
@@ -1,19 +1,19 @@
 package com.teemo.xuantruong.android_project.poster;
 
-import android.app.DownloadManager;
+
 import android.media.MediaPlayer;
-import android.net.Uri;
+
 import android.os.AsyncTask;
-import android.os.Build;
+
 import android.speech.tts.TextToSpeech;
-import android.support.annotation.RequiresApi;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.teemo.xuantruong.android_project.R;
 import com.teemo.xuantruong.android_project.connectJson.Get_apiText_to_speech;
@@ -23,48 +23,33 @@ import com.teemo.xuantruong.android_project.entity.Poster_entity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Locale;
-
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class Poster extends AppCompatActivity implements View.OnClickListener{
     private String TAG = Poster.class.getSimpleName();
-    private TextToSpeech speak;
-    private ImageButton imageBut ;
+
+    private ImageButton imageBut;
     private MyHttpHandler myhttp = new MyHttpHandler();
     private TextView name, content;
     private Poster_entity poster_entity =  new Poster_entity();
     private Get_apiText_to_speech getapiTexttospeech = new Get_apiText_to_speech();
     private  MediaPlayer mediaPlayer;
+    private  String text1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poster);
         imageBut= (ImageButton) findViewById(R.id.imageBut);
+
         name= (TextView) findViewById(R.id.name);
         content= (TextView) findViewById(R.id.content);
         mediaPlayer = new MediaPlayer();
-        //read mediaplayer online
+
+
         imageBut.setOnClickListener(this);
         MyAsyncTask myAsyncTask = new  MyAsyncTask();
         myAsyncTask.execute();
-        try {
-            mediaPlayer.setDataSource(poster_entity.getContent_poster());
-            mediaPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -82,15 +67,22 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
-        if(view.getId()== R.id.imageBut)
-        {
-            name.setText(poster_entity.getName_poster());
-            content.setText(poster_entity.getContent_poster());
+        if (view.getId() == R.id.imageBut) {
 
+            // play mediaplayer
+            if (mediaPlayer.isPlaying()) {
+                imageBut.setBackgroundResource(R.drawable.chat);
+                mediaPlayer.pause();
+            }
+            // pause mediaplayer
+            else {
+                imageBut.setBackgroundResource(R.drawable.speech_bubble);
+                mediaPlayer.start();
+            }
 
-            mediaPlayer.start();
         }
     }
+
 
     // json ->  object : show poster
     class  MyAsyncTask extends AsyncTask<Void,Void,Void>{
@@ -99,13 +91,20 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
                 try {
                     // get json poster
                     readJson();
+                    String txt = name.getText().toString()+ " "+content.getText().toString();
+//                    String txt ="ahihi do ngoc";
                     // get link file mp3
-                    String texturl = getapiTexttospeech.apiChangetexttomp3();
+                    String texturl = getapiTexttospeech.apiChangetexttomp3(txt);
                         // get json as json object
                     JSONObject jsonObject1 = new JSONObject(texturl);
                     // get elemet json object
-                    String text1 = jsonObject1.getString("async");
-                    poster_entity.setContent_poster(text1);
+                    text1 = jsonObject1.getString("async");
+
+                    // set mediaplayer in source
+                    //read mediaplayer online
+                    mediaPlayer.setDataSource(text1);
+                    mediaPlayer.prepare();
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e) {
