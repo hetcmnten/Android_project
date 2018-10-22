a75ce09892c483890300d166bf246bbaf96f848b
long <duclongph1997@gmail.com>
2018-10-22 14:46:16 +0700

    long

--- app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
+++ app/src/main/java/com/teemo/xuantruong/android_project/poster/Poster.java
@@ -41,11 +41,7 @@ import okhttp3.Response;
 public class Poster extends AppCompatActivity implements View.OnClickListener{
     private String TAG = Poster.class.getSimpleName();
     private TextToSpeech speak;
-    private int  result;
-    private EditText edit;
-    private String text;
     private ImageButton imageBut ;
-    private String url="";
     private MyHttpHandler myhttp = new MyHttpHandler();
     private TextView name, content;
     private Poster_entity poster_entity =  new Poster_entity();
@@ -56,27 +52,19 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poster);
         imageBut= (ImageButton) findViewById(R.id.imageBut);
-        edit= (EditText) findViewById(R.id.edit);
         name= (TextView) findViewById(R.id.name);
         content= (TextView) findViewById(R.id.content);
-        mediaPlayer= MediaPlayer.create(this, R.raw.lemin);
-
-
-        speak= new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
-                if (i == TextToSpeech.SUCCESS)
-                {
-                    result= speak.setLanguage(Locale.ENGLISH);
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"feature not support  in your device",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        mediaPlayer = new MediaPlayer();
+        //read mediaplayer online
         imageBut.setOnClickListener(this);
         MyAsyncTask myAsyncTask = new  MyAsyncTask();
         myAsyncTask.execute();
+        try {
+            mediaPlayer.setDataSource(poster_entity.getContent_poster());
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -88,7 +76,6 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onRestart() {
-
         mediaPlayer.start();
         super.onRestart();
     }
@@ -97,31 +84,16 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View view) {
         if(view.getId()== R.id.imageBut)
         {
-
-            text= edit.getText().toString().trim();
-//            speak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
             name.setText(poster_entity.getName_poster());
             content.setText(poster_entity.getContent_poster());
+
+
             mediaPlayer.start();
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        if (speak !=null)
-        {
-            speak.stop();
-            speak.shutdown();
-        }
-        super.onDestroy();
-    }
-
-    Response response=null;
     // json ->  object : show poster
     class  MyAsyncTask extends AsyncTask<Void,Void,Void>{
-
-
-            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
         protected Void doInBackground(Void... voids) {
                 try {
@@ -134,11 +106,6 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
                     // get elemet json object
                     String text1 = jsonObject1.getString("async");
                     poster_entity.setContent_poster(text1);
-                    // source file
-                    String file ="D:\\Crack\\ahihi.mp3";
-                    // download file
-                    download(text1,file);
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e) {
@@ -154,12 +121,10 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
             // get jproject
 //        String url1 = "http://192.168.0.104:8082/select_element.php?limit=10002";
         String jsonStr = myhttp.makeServiceCall(url1);
-
 //         json project
         if (jsonStr != null) {
             JSONArray aray = null;
             try {
-
                 // test
                 JSONObject jsonObject = new JSONObject(jsonStr);
                 JSONArray jsonArray= jsonObject.getJSONArray("contacts");
@@ -179,50 +144,5 @@ public class Poster extends AppCompatActivity implements View.OnClickListener{
                 e.printStackTrace();
             }
         }
-    }
-    DownloadManager downloadManager ;
-    // download file mp3
-    long queid;
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public  void  download(String link ,String file){
-        downloadManager= (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(link));
-         queid = downloadManager.enqueue(request);
-
-//       BufferedInputStream bis = null;
-//        try {
-//            File f = new File(file);
-//            if(!f.exists()) {
-//                f.createNewFile();
-//            }
-//            URL url = new URL(link);
-//            bis = new BufferedInputStream(url.openStream());
-//        FileOutputStream fis = new FileOutputStream(file);
-//        byte[] buffer = new byte[1024];
-//        int count=0;
-//        while((count = bis.read(buffer,0,1024)) != -1)
-//        {
-//            fis.write(buffer, 0, count);
-//        }
-//        fis.close();
-//        bis.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-    }
-    private  URL verify(String urls)
-    {
-        if (!urls.toLowerCase().startsWith("http://")){
-            return  null;
-        }
-      URL verifyurl=null;
-        try {
-            verifyurl = new URL(urls);
-        }catch (Exception e)
-        {
-
-        }
-        return  verifyurl;
     }
 }
